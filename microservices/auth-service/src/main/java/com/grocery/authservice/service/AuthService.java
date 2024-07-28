5347dd47e347019feaea074d49edb21328c19263
Vishnu <s.vishnuswaroop@gmail.com>
2024-07-28 16:42:58 +0530

    401 issue resolved

--- microservices/auth-service/src/main/java/com/grocery/authservice/service/AuthService.java
+++ microservices/auth-service/src/main/java/com/grocery/authservice/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.grocery.authservice.service;
 
-
 import com.grocery.authservice.entity.User;
 import com.grocery.authservice.entity.UserType;
 import com.grocery.authservice.repository.UserRepository;
@@ -12,34 +11,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    @Autowired
-    UserTypeRepository userTypeRepository;
+	@Autowired
+	UserTypeRepository userTypeRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
-    @Autowired
-    JwtService jwtService;
+	@Autowired
+	JwtService jwtService;
 
-    public String saveUser(User user) {
-        UserType userType = userTypeRepository.findById(user.getUserType().getId())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user type ID"));
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setUserType(userType);
-        userRepository.save(user);
+	public String saveUser(User user) {
+//        UserType userType = userTypeRepository.findById(user.getUserType().getId())
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid user type ID"));
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setUserType(user.getUserType());
+		userRepository.save(user);
+		return "User successfully added";
+	}
 
-        return "User successfully added";
-    }
+	public String generateToken(User user) {
+		return jwtService.generateToken(user);
+	}
 
-    public String generateToken(User user) {
-        return jwtService.generateToken(user);
-    }
+	public void validateToken(String token) {
+		jwtService.validateToken(token);
+	}
 
-    public void validateToken(String token) {
-        jwtService.validateToken(token);
-    }
-    
 }
--- microservices/product-service/src/main/java/com/grocery/productservice/entity/Product.java
+++ microservices/product-service/src/main/java/com/grocery/productservice/entity/Product.java
@@ -1,16 +1,9 @@
 package com.grocery.productservice.entity;
 
-import java.util.Set;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class Product {
@@ -52,7 +45,7 @@ public class Product {
         return categoryId;
     }
 
-    public void setCategory(Long categoryId) {
+    public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
     }
 
